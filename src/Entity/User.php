cf820b3497d0afaf6ad55176fe5723f4596ce7cd
Alan <bwb.dufrenoy.a@fondespierre.com>
2026-01-19 10:43:55 +0100

    Suppression de User (UserController.php)

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -6,6 +6,10 @@ use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+
+
+
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 class User
@@ -159,6 +163,11 @@ class User
         $this->mot_de_passe = $mot_de_passe;
 
         return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->mot_de_passe;
     }
 
     public function isPermisDeConduire(): ?bool
